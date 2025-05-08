0bc853e383cde34026c4679f9c6f9f8ba963afa2
iamanggi <anggipuspita452@gmail.com>
2025-05-08 20:38:49 +0700

    add checkbox

--- lib/pesentation/todo.dart
+++ lib/pesentation/todo.dart
@@ -145,7 +145,14 @@ class TodoPage extends StatelessWidget {
                                         ),
                                       )
                                     ],
-                                  )
+                                  ),
+                                  Checkbox(
+                                    value: todo.isCompleted,
+                                      onChanged: (value){
+                                      context.read<TodoBloc>().add(
+                                        TodoEventComplete(index: index)
+                                      );
+                                    })
                                 ],
                               ),
                             );
