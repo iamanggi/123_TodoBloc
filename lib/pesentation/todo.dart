ac3f1c3d72f90c933e160a29bacd03d135fadab6
iamanggi <anggipuspita452@gmail.com>
2025-05-08 20:09:51 +0700

    add show selected date with BlocBuilder

--- lib/pesentation/todo.dart
+++ lib/pesentation/todo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/bloc/todo_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -19,6 +21,17 @@ class TodoPage extends StatelessWidget {
                   Column(
                     children: [
                       Text('Selected Date'),
+                      BlocBuilder<TodoBloc, TodoState>
+                      (builder: (context, state){
+                        if(state is TodoLoaded){
+                          if (state.selectedDate != null){
+                            return Text(
+                              '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                            );
+                          }
+                        }
+                        return Text('No date selected');
+                      })
                     ],
                   )
                 ],
