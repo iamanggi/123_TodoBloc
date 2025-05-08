48694c820967e4bf9417a455a420daa40cbfb60e
iamanggi <anggipuspita452@gmail.com>
2025-05-08 20:30:07 +0700

    add display todo list with loading and empty state

--- lib/pesentation/todo.dart
+++ lib/pesentation/todo.dart
@@ -92,7 +92,34 @@ class TodoPage extends StatelessWidget {
                         } , child: Text('Tambah')
                         )
                   ],
-                ))
+                )),
+                SizedBox(height: 16.0),
+                Expanded(
+                  child: BlocBuilder<TodoBloc, TodoState>(
+                    builder: (context, state){
+                      if (state is TodoLoaded){
+                        return Center(child: CircularProgressIndicator());
+                      } else if (state is TodoLoaded){
+                        if (state.todos.isEmpty){
+                          return Center(child: Text('Todo List is empty'));
+                        }
+                        return ListView.builder(
+                          itemCount: state.todos.length,
+                          itemBuilder: (context, index) {
+                            final todo = state.todos[index];
+                            return Container(
+                              margin: EdgeInsets.only(bottom: 8.0),
+                              padding: EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: Colors.blue[50],
+                                borderRadius: BorderRadius.circular(8.0)
+                              ),
+                            );
+                          });
+                      }
+                    }
+                    )
+                  )
             ],
           ),
           )),
